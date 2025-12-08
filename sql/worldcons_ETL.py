d8a7800af9b1222080998f98f8a7d6b96d0912ad
Sri Gana Randhi <gana.randhi@gmail.com>
2025-12-08 16:11:17 -0500

    Updating World Energy and ETL to DB using python

---
+++ sql/worldcons_ETL.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import os
+import glob
+import psycopg2
+from sqlalchemy import create_engine
+import io
+import csv
+
+
+joined_files = os.path.join("C:\RenewableEnergyAI\RenewableEnergyRevolution\data\World_Consumption", "*.csv")
+joined_list = glob.glob(joined_files)
+
+    # Using a list comprehension
+list_of_dfs = [pd.read_csv(filename) for filename in joined_list]
+combined_df = pd.concat(list_of_dfs, ignore_index=True)
+
+data = pd.DataFrame(combined_df)
+
+print(combined_df.head())
+# print(combined_df.info())
+print(combined_df.describe())
+# print(data.shape)
+print(len(data))
+
+# --- Database Connection Details ---
+DB_NAME = "energy"
+DB_USER = "postgres"
+DB_PASSWORD = "123456"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+
+
+# --- Connect to PostgreSQL and Read Sample Data ---
+conn = psycopg2.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASSWORD)
+conn_string = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+db_engine = create_engine(conn_string)
+
+# --- Import DataFrame into PostgreSQL ---
+try:    
+    print(f"Please Truncate table before running twice.")
+    combined_df.to_sql('worldconsumption', db_engine, if_exists='append', index=False)
+    print(f"Successfully imported data from CSVs into '{'worldconsumption'}' table.")
+    select_query = f"SELECT * from worldconsumption WHERE Country = 'India' LIMIT 5;"
+    df= pd.read_sql(select_query, db_engine)
+    print(df.head())
+except Exception as e:
+    print(f"Error importing data: {e}")
+
+# Optional: Close the connection
+db_engine.dispose()
+conn.close()
